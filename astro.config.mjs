05715adb2ed8e0906cd7ddcd4f6b030b6332f934
yahircreativo <yahir.creativo@gmail.com>
2026-07-11 18:07:36 -0700

    Manufacturers-section

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://yahircreativo-art.github.io',
-    base: '/H-CProductions/',
+    base: '/H-CProductions',
     vite: {
         plugins: [tailwindcss()],
     }
